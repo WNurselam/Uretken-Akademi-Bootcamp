9acbcf63e673d26b5c39d25b5109b74c028e39dc
Nurselam SAYAN <77880259+WNurselam@users.noreply.github.com>
2023-03-05 17:33:34 +0300

    Update Celcius.jsx

--- temperature-project/src/Celcius.jsx
+++ temperature-project/src/Celcius.jsx
@@ -3,13 +3,9 @@ import { Col } from "reactstrap";
 
 const Celcius = ({ temperature }) => {
   const [celcTemp, setCelcTemp] = useState(0);
-  
-  
-
   return (
     <Col className="m-3 border p-5 shadow">
       <p>Celcius {celcTemp + temperature} °C </p>
-
         {
           celcTemp + temperature < 25 ? <span>Soğuk</span>:"Sıcak "
         }
